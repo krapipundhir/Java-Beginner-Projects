1e01e3482829f84052f3ace365791b766492adb2
Krapi Pundhir <krapipundhir6@gmail.com>
2026-08-02 18:42:50 +0530

    Added my one more GCD file to calculate Greatest Common divisor of two numbers by euclidian algo

---
+++ 04-Functions/GreatestDivisor.java
@@ -0,0 +1,31 @@
+// Solving GCD problem by using Euclidian Algorithm.
+import java.util.*;
+
+public class GreatestDivisor {
+    public static void commonGreatestDivisor(int a, int b) {
+       
+        while (a!=b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+
+        }
+        System.out.print("GCD is: " + a);
+        return;
+
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First number: ");
+        int First = sc.nextInt();
+        System.out.print("Enter Second number: ");
+        int Second = sc.nextInt();
+
+        commonGreatestDivisor(First, Second);
+        return;
+    }
+
+}
