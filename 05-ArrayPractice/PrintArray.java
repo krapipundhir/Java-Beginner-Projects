9aa5b9a96130878ec3ef9f0eee361c54bd35100b
Krapi Pundhir <krapipundhir6@gmail.com>
2026-08-13 22:01:25 +0530

    Added my New File to print Array.

---
+++ 05-ArrayPractice/PrintArray.java
@@ -0,0 +1,12 @@
+
+
+public class PrintArray {
+    public static void main(String args[]) {
+        int numbers[] = { 95, 89, 90 };
+        for (int i = 0; i < 3; i++) {
+            System.out.println(numbers[i]);
+        }
+
+    }
+
+}
