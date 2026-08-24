cc2de15b2421f585dc38850aedcc7ec6318e6974
Krapi Pundhir <krapipundhir6@gmail.com>
2026-08-24 11:39:00 +0530

    Added my new File of Compare Method of Java.

---
+++ 06-Strings/CompareMtd.java
@@ -0,0 +1,16 @@
+// A Program to Compare Strings.
+public class CompareMtd {
+    public static void main(String args[]) {
+        String name1 = "Krapi";
+        String name2 = "Shrushti";
+        if (name1.equals(name2)) {
+            System.out.print("They are the same Strings.");
+        } else {
+            System.out.print("They are the different Strings.");
+        }
+        // Do'nt Use == to check String equality.
+        // Because it gives Correct,Incorrect Answeres in different differnt Conditions.
+
+    }
+
+}
