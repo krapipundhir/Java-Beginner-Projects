7c08ec27f65a6fe0c61aba0392c97abc0b9cba5f
Krapi Pundhir <krapipundhir6@gmail.com>
2026-09-16 20:25:48 +0530

    Add: Program to Print UserName from the Email-Id.

---
+++ 06-Strings/GetUsername.java
@@ -0,0 +1,18 @@
+// A Program to print Username from the Email id given by the User.
+import java.util.Scanner;
+public class GetUsername {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String email=sc.next();
+        String username="";
+        for(int i=0;i<email.length();i++){
+            if(email.charAt(i)=='@'){
+            break;
+            }
+            else{
+                username+=email.charAt(i);
+            }
+        }
+        System.out.println(username);
+    }
+}
