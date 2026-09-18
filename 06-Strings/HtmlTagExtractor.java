1e68fc9354b385648e7ca65ce0b62466b55154c8
Krapi Pundhir <krapipundhir6@gmail.com>
2026-09-18 19:55:13 +0530

    Add: A Program that Extracts html Tag from the User's Input.

---
+++ 06-Strings/HtmlTagExtractor.java
@@ -0,0 +1,36 @@
+//  * Project: Java Practice / HackerRank Solutions
+//  * File: TagContentExtractor.java
+//  * Description: Extracts valid text content enclosed between matching HTML/XML tags
+//  *              using Java Regular Expressions (Regex) and Pattern/Matcher classes.
+//  * Author: [Krapi Pundhir]
+ 
+import java.util.*;
+import java.util.regex.*;
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class HtmlTagExtractor {
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int testCases=sc.nextInt();
+    sc.nextLine();
+    String regex = "<(.+?)>([^<]+)</\\1>";
+    Pattern pattern=Pattern.compile(regex);
+    while(testCases>0){
+        String input=sc.nextLine();
+        Matcher matcher=pattern.matcher(input);
+        boolean found=false;
+        while(matcher.find()){
+            System.out.println(matcher.group(2));
+            found =true;
+        }
+        if(!found){
+            System.out.println("None");
+        }
+        testCases--;
+    }
+    sc.close();
+
+    }
+}
